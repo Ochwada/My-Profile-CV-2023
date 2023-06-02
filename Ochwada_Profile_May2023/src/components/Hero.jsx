1d32a3c90d5c123a9b32e098a16d47a9566ed77c
Ochwada <Ochwada@users.noreply.github.com>
2023-06-02 15:47:49 +0200

    hero file -done

--- Ochwada_Profile_May2023/src/components/Hero.jsx
+++ Ochwada_Profile_May2023/src/components/Hero.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import { motion } from 'framer-motion';
+
+
+
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto' >
+    <section className={`relative w-full h-screen mx-auto`}>
       <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -18,12 +20,31 @@ const Hero = () => {
               Linda
             </span> </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            The <span className="text-pink-200">Tech Evangelist </span> <br className="sm:block hidden"/>
+            The <span className="text-pink-200">Tech Evangelist </span> <br className="sm:block hidden" />
             Empowering Tomorrow's Innovations, Today!
           </p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-0 bottom-12 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
