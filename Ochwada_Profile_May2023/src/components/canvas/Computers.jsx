6a590002cb29a99ec0f9c9401245c70c16002c86
Ochwada <Ochwada@users.noreply.github.com>
2023-06-02 13:12:08 +0200

    loading 3D

--- Ochwada_Profile_May2023/src/components/canvas/Computers.jsx
+++ Ochwada_Profile_May2023/src/components/canvas/Computers.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF, Html } from '@react-three/drei';
+
+
+import CanvasLoader from '../Loader';
+
+
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
 
-export default Computers
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} position={[0, 3, 0]} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minAzimuthAngle={Math.PI / 2} />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
